3933c31c7591618d409d713556501bc81c83599f
santoshmondal <santosh.ece06@gmail.com>
2023-03-03 10:04:27 +0530

    initial commit.

---
+++ src/main/java/in/cdac/MySpringBootProjectApplication.java
@@ -0,0 +1,22 @@
+package in.cdac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MySpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootProjectApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World!";
+	}
+}
+
+
