8d4a11257eda2ce5fff91cea9ba1780d3a3033f6
santoshmondal <santosh.ece06@gmail.com>
2023-03-03 12:12:46 +0530

    b6 - lombok and object. Cleanup.

--- src/main/java/in/cdac/contorller/GreetingController.java
+++ src/main/java/in/cdac/contorller/GreetingController.java
@@ -3,6 +3,8 @@ package in.cdac.contorller;
 import in.cdac.modal.Greeting;
 import in.cdac.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,12 @@ public class GreetingController {
     @GetMapping("/message")
     public Greeting getGreetingObjectV1() {
         return greetingService.getGreetingObjectV3();
+    }
+
+    @GetMapping("/message1")
+    public ResponseEntity<Greeting> getGreetingObjectV2() {
+        Greeting greetingObject = greetingService.getGreetingObjectV3();
+        return new ResponseEntity<>(greetingObject, HttpStatus.CREATED);
     }
 
 }
