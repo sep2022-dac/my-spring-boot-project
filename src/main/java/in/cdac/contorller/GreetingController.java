c5de096f3b9e482be57130d146cd4aa2ea53dba3
santoshmondal <santosh.ece06@gmail.com>
2023-03-07 09:17:31 +0530

    cross origin added.

--- src/main/java/in/cdac/contorller/GreetingController.java
+++ src/main/java/in/cdac/contorller/GreetingController.java
@@ -5,6 +5,7 @@ import in.cdac.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
